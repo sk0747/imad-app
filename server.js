4c6d3ec350f6fd436d1d23469d2875fac61599f5
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-02-18 00:39:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/Article-one',function(req,res){
-    res.send('Article one is requested and we will serve soon');
+    res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 app.get('/Article-two',function(req,res){
     res.send('Article two is requested and we will serve soon');
