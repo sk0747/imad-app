afb83aa03cf81538dbde9cad6bb3368bb6f96839
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-02-17 13:18:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/Article-one',function(req,res){
+    res.send('Article one is requested and we will serve soon');
+});
+app.get('/Article-two',function(req,res){
+    res.send('Article two is requested and we will serve soon');
+});
+app.get('/Article-three',function(req,res){
+    res.send('Article three is requested and we will serve soon');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
