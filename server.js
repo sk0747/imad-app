706d1f609131062636ad59815d7efab785287988
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-02-18 21:53:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/Article-one',function(req,res){
-    res.send(createTemplate(Articleone));
+    res.send(createTemplate(articleone));
 });
 app.get('/Article-two',function(req,res){
     res.send('Article two is requested and we will serve soon');
