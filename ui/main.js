b56522bd495a9554e1e29d7613fbe3596bc76e83
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-02-22 11:03:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
+//moventhe image
+var img=document.getElementById('img');
+img.onClick = function(){
+    img.style.marginLeft='100px';
+}
