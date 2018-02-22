e7d047ce1bda54e360b5f5427aaceb3fc09f02e7
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-02-23 00:45:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,16 @@ button.onclick=function(){
     request.send(null);
     
 };
+var nameInput=document.getElementById('name');
+var name = nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    var name=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<name.length;i++){
+        list+='<li>' + name[i] + '</li>';
+        
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+};
