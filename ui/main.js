89fd612018ba7f64432a23268ec60ed2320dfa33
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-02-24 19:58:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,6 +24,28 @@ var nameInput=document.getElementById('name');
 var name = nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
+     var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+              var names=request.responseText;
+              names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<name.length;i++){
+                 list+='<li>' + name[i] + '</li>';
+        
+                     }
+                var ul=document.getElementById('namelist');
+                 ul.innerHTML=list; 
+            }
+            
+        }
+    };
+    request.open('GET','http://skr001998.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
     var name=['name1','name2','name3','name4'];
     var list='';
     for(var i=0;i<name.length;i++){
