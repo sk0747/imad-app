6b4b4a59dd0c34f912162486e55b18a8c983f1d7
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-02-24 21:00:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,7 +43,9 @@ submit.onclick=function(){
             }
             
         }
-}};
+};
+  request.open('GET','http://skr001998.imad.hasura-app.io/submit-name?name='+name,true);
+   request.send(null);  
+};
 
-    request.open('GET','http://skr001998.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    
