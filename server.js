353b7ba352b94240f9a21dfd1c8cc30c5218a105
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-02-24 18:43:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/ui/madi.png', function (req, res) {
 var names=[];
 app.get('/submit-name/:name',function(req,res){
     var name=req.params.name;
-    name.push(name);
+    names.push(name);
     res.send(JSON.stringify(name));
 });
 
