cb9eb5af67288bd6e3b1ce0f77a9d978efbb979c
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-03-10 12:01:38 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool=require('pg').pool;
+var Pool=require('pg').Pool;
 
 
 var config={
@@ -67,7 +67,7 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool=new pool(config);
+var pool= new Pool(config);
 app.get('/test_db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
         if(err){
