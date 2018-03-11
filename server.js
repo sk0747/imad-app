89c5591eb64a46b9077ec12d05f02ec4c7f6c655
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-03-11 05:17:03 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,9 +103,21 @@ app.get('/counter',function(req,res){
     counter=counter + 1;
     res.send(counter.toString());
 });
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName',function(req,res){
+    
+    pool.query("SELECT * FROM article WHERE title= '" + res.params.articleName + "'",function(err,result){
+      if(err){
+          res.send(500).send(err.toString());
+      }  
+      else{
+          if(result.rows.length===0){
+              res.status(404).send('article not found');
+          }else{
+          var articleData=result.rows[0];
+           res.send(createTemplate(articlesData));    
+          }
+      }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
