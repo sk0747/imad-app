00bf3ce92711a18586085896f07df6d24dd1457d
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-03-14 12:22:53 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 
 
 var config={
@@ -52,6 +53,20 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+function hash (input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req.params.input,salt,'this-is-some-random-string ');
+    res.send(hashedString);
+});
+
+
+
+
 var pool= new Pool(config);
 app.get('/test_db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
@@ -63,6 +78,11 @@ app.get('/test_db',function(req,res){
         
     });
 });
+
+
+
+
+
 
 var counter=0;
 app.get('/counter',function(req,res){
