24fbc8d6ec699fcf6e9df26a0712b38569e118b1
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-03-15 10:07:05 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.post('/login',function(req,res){
                 var salt=dbString.split('$')[2];
                 var hashedpassword=hash(password,salt);
                 if(hashedpassword===dbString){
-                    req.session.auth={userId:result.rows[0],id};
+                    req.session.auth={userId:result.rows[0].id};
                     
                     
                     
