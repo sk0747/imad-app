133c62ad955e6c56f7a9cf833530281167c49c04
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-03-15 07:58:55 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,4 @@
-var button=document.getElementById('counter');
-button.onclick=function(){
-    
-    //make a request to the counter
-    var request = new XMLHttpRequest();
-    
-    //capture the response and store it in a variable
-    
-    request.onreadystatechange = function(){
-        if (request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
-            }
-            
-        }
-    };
-    request.open('GET','http://skr001998.imad.hasura-app.io/counter',true);
-    request.send(null);
-    
-};
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
      var request = new XMLHttpRequest();
@@ -29,23 +8,25 @@ submit.onclick=function(){
     request.onreadystatechange = function(){
         if (request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-              var names=request.responseText;
-              names=JSON.parse(names);
-                var list='';
-                for(var i=0;i<names.length;i++){
-                 list+='<li>' + names[i] + '</li>';
-        
-                     }
-                var ul=document.getElementById('namelist');
-                 ul.innerHTML=list; 
+             console.log('user logged in');
+             alert('logged in is sucessfully');
+             
+            }else if(request.status===403){
+              alert('username/password is incorrect')  ;
+            }else if(request.status===500){
+            alert('something went wrong on the server');  
             }
             
         }
 };
-   var nameInput=document.getElementById('name');
-   var name = nameInput.value; 
-   request.open('GET','http://skr001998.imad.hasura-app.io/submit-name?name='+name,true);
-   request.send(null);  
+   var username=document.getElementById('username').value;
+     var password=document.getElementById('password').value;
+     console.log(username);
+     console.log(password);
+ 
+   request.open('POST','http://skr001998.imad.hasura-app.io/login',true);
+   request.setRequestHeader('Content-type','application/json');
+   request.send(JSON.stringfy({username: uaername,password: password}));  
 };
 
     
