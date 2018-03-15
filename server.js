488e14262fcff1fd14eef068dd2a88985cf543b2
shashi kumar <36528856+sk0747@users.noreply.github.com>
2018-03-15 06:13:55 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/hash/:input',function(req,res){
     res.send(hashedString);
 });
 
-app.post('/creat-user',function(req,res){
+app.post('/create-user',function(req,res){
     var username=req.body.username;
     var password=req.body.password;
     var salt=crypto.randomBytes(128).toString('hex');
